0df731b8831994f1b0a9fb8c23878d90a9046ee6
xiaoyang <532758463@qq.com>
2022-04-23 21:06:32 +0800

    属性绑定

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,26 +6,32 @@ import PropsDemoVue from './PropsDemo.vue'
 import ReactiveDemoVue from './ReactiveDemo.vue'
 import SlotDemoVue from './SlotDemo.vue'
 import ComputedDemoVue from './ComputedDemo.vue'
+import ClassBindingVue from '@/components/ClassBinding.vue';
+import Form from '@/components/Form.vue'
 
 const tabs = {
   PropsDemoVue,
   ReactiveDemoVue,
   SlotDemoVue,
-  ComputedDemoVue
+  ComputedDemoVue,
+  ClassBindingVue,
+  Form
 }
 
 const tabNames = {
   PropsDemoVue: "Props",
   ReactiveDemoVue: 'reactive',
   SlotDemoVue: '插槽',
-  ComputedDemoVue: '计算属性'
+  ComputedDemoVue: '计算属性',
+  ClassBindingVue: '属性绑定',
+  Form: '表单'
 }
 
 const currentTab = ref('PropsDemoVue')
 </script>
 <template>
   <FancyButton v-for="(_, tab) in tabs" @click="currentTab = tab">
-    {{ tabNames[tab] }}
+    {{ tabNames[tab] || tab }}
     <AwesomeIcon v-if="currentTab === tab" />
   </FancyButton>
   <component :is="tabs[currentTab]" class="tab"></component>
