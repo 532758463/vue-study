cf4c2161cc7dc0dd24619afc9d719a6710fbfb7f
徐小杨 <xuxyyf@seeyon.com>
2025-03-15 15:47:51 +0800

    chore: 类型问题

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -3,7 +3,11 @@ import { App } from 'vue';
 export default {
   install:(app: App, options: Record<string, any>) => {
     app.config.globalProperties.$translate = (key: string) => {
-      return key.split('.').reduce((o, i) => o[i], options)
+      const a = key.split('.').reduce((o, i) => o[i], options);
+      if(typeof a === 'string') {
+        return a;
+      }
+      return '';
     }
   }
 }
