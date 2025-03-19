ade603ddd8d77db570da5f152a757f163bb50da5
徐小杨 <xuxyyf@seeyon.com>
2025-03-19 09:30:46 +0800

    feat: css grid

--- src/pages/PropsDemo.vue
+++ src/pages/PropsDemo.vue
@@ -23,7 +23,38 @@ function add(c: number) {
     <BlogPost :count="count" v-for="post in posts" :key="post.id" :title="post.title" :title2="post.title + post.id"
       @enlarge-text="postFontSize += 0.1" @add="add"></BlogPost>
     {{ $translate('greetings.hello') }}
+    <div class="container">
+      <div>1</div>
+      <div class="item2">2</div>
+      <div class="item3">3</div>
+      <div class="item4">4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: 20px 1fr;
+  row-gap: 20px;
+  column-gap: 20px;
+
+
+  div {
+    border: 1px solid green;
+    background-color: lime;
+  }
+  .item2 {
+    grid-column-end:span 3;
+  }
+  .item3 {
+    grid-column: 1;
+  }
+}
+</style>
